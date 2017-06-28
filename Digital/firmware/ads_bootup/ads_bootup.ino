e39c93bd3ea26931ce0738776887f67fdab4bd10
a1bertle <albert.le6446@gmail.com>
2017-06-27 23:36:34 -0400

    Updated ads_bootup code to use modified ADS1299 library

--- Digital/firmware/ads_bootup/ads_bootup.ino
+++ Digital/firmware/ads_bootup/ads_bootup.ino
@@ -1,3 +1,5 @@
+
+
 /*Developed by Joel Murphy and Conor Russomanno (Summer 2013)
   This example uses the ADS1299 Arduino Library, a software bridge between the ADS1299 chip and 
   Arduino. See http://www.ti.com/product/ads1299 for more information about the device and the README
@@ -35,23 +37,28 @@ int pinPWDN = 6; // ADS input
 
 void setup() {
    pinMode(pinCLKSEL, OUTPUT);
-   pinMode(pinPWDN, OUTPUT);
-   
+   digitalWrite(pinCLKSEL, HIGH); // Set CLKSEL to high because using internal oscillator
 
-   digitalWrite(pinCLKSEL, HIGH);
-   digitalWrite(pinPWDN, HIGH);
-   
-  // don't put anything before the initialization routine for recommended POR  
-  ADS.initialize(pinDRDY, pinRESET, pinCS, 4, false); // (DRDY pin, RST pin, CS pin, SCK frequency in MHz, Daisy_en = 0);
+  // (DRDY pin, RST pin, CS pin, PWDN pin, SCK frequency in MHz, Daisy_en = 0);
+  ADS.initialize(pinDRDY, pinRESET, pinCS, pinPWDN, 4, false); // (DRDY pin, RST pin, CS pin, SCK frequency in MHz, Daisy_en = 0);
 
   Serial.begin(115200);
   Serial.println("ADS1299 Bootup"); 
-  delay(1000);             
+  //delay(50);             
 
   ADS.verbose = true;      // when verbose is true, there will be Serial feedback 
-  ADS.RESET();             // all registers set to default
-  ADS.SDATAC();            // stop Read Data Continuous mode to communicate with ADS
-  ADS.RREGS(0x00,0x17);    // read ADS registers starting at 0x00 and ending at 0x17
+  //ADS.RESET();             // all registers set to default
+  //ADS.SDATAC();            // stop Read Data Continuous mode to communicate with ADS
+  
+  digitalWrite(pinCS, LOW);
+  ADS.transfer(0x00);
+  byte data = ADS.transfer(0x00);
+  digitalWrite(pinCS, HIGH);
+  Serial.println(data);
+  
+  //ADS.getDeviceID();
+
+  //ADS.RREGS(0x00,0x17);    // read ADS registers starting at 0x00 and ending at 0x17
   //ADS.WREG(CONFIG3,0xE0);  // enable internal reference buffer
   //ADS.RREG(CONFIG3);       // verify write
   //for(byte i=CH1SET; i<=CH8SET; i++){   // set up to modify the 8 channel setting registers
@@ -64,4 +71,6 @@ void setup() {
 } // end of setup
 
 void loop(){
+  //delay(5000);
+  //Serial.println("Waiting");
 } // end of loop
