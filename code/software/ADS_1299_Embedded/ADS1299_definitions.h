497579f51b5ae150ff4c70ae19b066b8c914a674
a1bertle <albert.le6446@gmail.com>
2017-11-12 11:38:39 -0500

    Added ese519 library files with comments

---
+++ code/software/ADS_1299_Embedded/ADS1299_definitions.h
@@ -0,0 +1,212 @@
+// ADS1299 Constants
+#ifndef _Definitions_h
+#define _Definitions_h
+
+// Board specific settings
+// fclk = 2.048 Mhz, TCLK = 488.2815 nanoseconds	
+#define TCLK 		0.4882815 	// microseconds
+#define FCLK 		2.048
+#define __DEBUG__
+
+
+// 
+#define PIN_DRDY  RPI_BPLUS_GPIO_J8_15 	// Make Pin 22 the DRDY pin
+#define PIN_CS    RPI_BPLUS_GPIO_J8_24  // Make Pin 24 (CE0) the CS pin
+#define PIN_RESET RPI_BPLUS_GPIO_J8_18  // Make Pin 18 the RESET pin
+
+// Settings spefific to our project
+#define SPS            250  // 250 samples per sample
+#define VREF	       4.5  // 
+#define DVDD	       3.0  // Digital VDD
+#define AVDD	       5.0  // Analog VDD
+
+////////////////////// SPI ///////////////////////////////////////////
+
+//SPI Command Definition Byte Assignments (Datasheet, pg. 35)
+#define _WAKEUP        0x02 // Wake-up from standby mode
+#define _STANDBY       0x04 // Enter Standby mode
+#define _RESET         0x06 // Reset the device
+#define _START         0x08 // Start and restart (synchronize) conversions
+#define _STOP          0x0A // Stop conversion
+#define _RDATAC        0x10 // Enable Read Data Continuous mode (default mode at power-up)
+#define _SDATAC        0x11 // Stop Read Data Continuous mode
+#define _RDATA         0x12 // Read data by command; supports multiple read back
+
+#define _RREG          0x20 // (also = 00100000) is the first opcode that the address must be added to for RREG communication
+#define _WREG          0x40 // 01000000 in binary (Datasheet, pg. 35)
+
+//////////////// ADS1299 Control Register Base Addresses /////////////////////////////
+
+// ID:     ID Control Register
+// The read-only ID control register is programmed during device manufacture to indicate device characteristics (pg. 45)
+// Reset: 0xXE
+// Fields:
+//	[7:5]	REV_ID 
+//	[4:4] 	Reserved
+//	[3:2] 	DEVICE_ID
+//	[1:0] 	NU_CH
+#define ID             0x00 
+
+// CONFIG1: Configuration Register 1
+// This register configures the !DAISY_EN bit, clock, and data rate. (pg. 46)
+// Reset: 0x96
+// Fields:
+//	[7:7]	Reserved
+//	[6:6]  !DAISY_EN
+//	[5:5]	CLK_EN
+//	[4:3]	Reserved
+//	[2:0]	DR
+#define CONFIG1        0x01
+
+// CONFIG2: Configuration Register 2
+// This register configures the test signal generation (pg. 47)
+// Reset: 0xC0
+// Fields:
+//	[7:5]	Reserved
+//	[4:4]	INT_CAL
+//	[3:3]	Reserved
+//	[2:2]	CAL_AMP
+//	[1:0]	CAL_FREQ
+#define CONFIG2        0x02
+
+// CONFIG3: Configuration Register 3
+// Configuration register 3 configures either an internal or exteral reference and BIAS operation. (pg. 48)
+// Reset: 0x60
+// Fields:
+//	[7:7]	!PD_REFBUF
+//	[6:5]  	Reserved
+//	[4:4]	BIAS_MEAS
+//	[3:3]	BIASREF_INT
+//	[2:2]	!PD_BIAS
+//	[1:1]	BIAS_LOFF_SENS
+//	[0:0]	BIAS_STAT
+#define CONFIG3        0x03
+
+// LOFF: Lead-Off Control Register
+// The lead-off control register configures the lead-off detection operation. (pg. 49)
+// Reset: 0x00
+// Fields:
+//	[7:5]	COMP_TH
+//	[4:4]  	Reserved
+//	[3:2]	ILEAD_OFF
+//	[1:0]	FLEAD_OFF
+#define LOFF           0x04
+
+// CHnSET: Individual Channel Settings (n = 1 to 8)
+// The CH[1:8]SET control register configures the power mode, PGA gain, and multiplexer settings channels. See
+// the Input Multiplexer section for details. CH[2:8]SET are similar to CH1SET, corresponding to the respective
+// channels. (pg. 50)
+// Reset: 0x61
+// Fields:
+//	[7:7]	PDn
+//	[6:4]  	GAINn
+//	[3:3]	SRB2
+//	[2:0]	MUXn
+#define CH1SET         0x05
+#define CH2SET         0x06
+#define CH3SET         0x07
+#define CH4SET         0x08
+#define CH5SET         0x09
+#define CH6SET         0x0A
+#define CH7SET         0x0B
+#define CH8SET         0x0C
+
+// BIAS_SENSP: Bias Drive Positive Derivation Register
+// This register controls the selection of the positive signals from each channel for bias voltage (BIAS) derivation.
+// See the Bias Drive (DC Bias Circuit) section for details. (pg. 51)
+// Reset: 0x00
+// Fields:
+//	[n]		BIASP(n+1)
+#define BIAS_SENSP     0x0D
+
+// BIAS_SENSP: Bias Drive Positive Derivation Register
+// This register controls the selection of the negative signals from each channel for bias voltage (BIAS) derivation.
+// See the Bias Drive (DC Bias Circuit) section for details. (pg. 52)
+// Reset: 0x00
+// Fields:
+//	[n]		BIASN(n+1)
+#define BIAS_SENSN     0x0E
+
+// LOFF_SENSP: Positive Signal Lead-Off Detection Register
+// This register selects the positive side from each channel for lead-off detection. See the Lead-Off Detection
+// section for details. The LOFF_STATP register bits are only valid if the corresponding LOFF_SENSP bits are set
+// to 1. (pg. 53)
+// Reset: 0x00
+// Fields:
+//	[n]		LOFFP(n+1)
+#define LOFF_SENSP     0x0F
+
+// LOFF_SENSN: Negative Signal Lead-Off Detection Register
+// This register selects the negative side from each channel for lead-off detection. See the Lead-Off Detection
+// section for details. The LOFF_STATN register bits are only valid if the corresponding LOFF_SENSN bits are set
+// to 1. (pg. 54)
+// Reset: 0x00
+// Fields:
+//	[n]		LOFFP(n+1)
+#define LOFF_SENSN     0x10
+
+
+// LOFF_FLIP: Lead-Off Flip Register
+// This register controls the direction of the current used for lead-off derivation. See the Lead-Off Detection section
+// for details. (pg. 55)
+// Reset: 0x00
+// Fields: 
+//	[n]		LOFF_FLIP(n+1)
+#define LOFF_FLIP      0x11
+
+// LOFF_STATP: Lead-Off Positive Signal Status Register
+// This register stores the status of whether the positive electrode on each channel is on or off. See the Lead-Off
+// Detection section for details. Ignore the LOFF_STATP values if the corresponding LOFF_SENSP bits are not set
+// to 1.
+// Reset: 0x00
+// Fields:
+//	[n] IN(n+1)P_OFF
+#define LOFF_STATP     0x12
+
+// LOFF_STATN: Lead-Off Negative Signal Status Register
+// This register stores the status of whether the negative electrode on each channel is on or off. See the Lead-Off
+// Detection section for details. Ignore the LOFF_STATN values if the corresponding LOFF_SENSN bits are not set
+// to 1.
+// Reset: 0x00
+// Fields:
+//	[n] IN(n+1)N_OFF
+#define LOFF_STATN     0x13
+
+// GPIO: General-Purpose I/O Register
+// The general-purpose I/O register controls the action of the three GPIO pins. When RESP_CTRL[1:0] is in mode
+// 01 and 11, the GPIO2, GPIO3, and GPIO4 pins are not available for use.
+// Reset: 0x0F
+// Fields:
+//	[7:4]	GPIOD
+//	[3:0] 	GPIOC
+#define GPIO           0x14
+
+// MISC1: Miscellaneous 1 Register
+// This register provides the control to route the SRB1 pin to all inverting inputs of the four, six, or eight channels
+// (ADS1299-4, ADS1299-6, or ADS1299).
+// Reset: 0x00
+// Fields:
+//	[7:6]	Reserved
+//	[5:5] 	SRB1
+//	[4:0] 	Reserved
+#define MISC1          0x15
+
+// MISC2: General-Purpose I/O Register
+// This register is reserved for future use.
+// Reset: 0x00
+// Fields:
+//	[7:0]	Reserved
+#define MISC2          0x16
+
+// CONFIG4: Configuration Register 4
+// This register configures the conversion mode and enables the lead-off comparators.
+// Reset: 0x00
+// Fields:
+//	[7:4]	Reserved
+//	[3:3]	SINGLE_SHOT
+//	[2:2]	Reserved
+//	[1:1]	!SINGLE_SHOT
+//	[0:0]	Reserved
+#define CONFIG4        0x17
+
+#endif
---
+++ code/software/ADS_1299_Embedded/ads1299.c
@@ -0,0 +1,177 @@
+// ads1299.c
+// ADS1299 library
+// Product Page - http://www.ti.com/product/ads1299
+// Datasheet - http://www.ti.com/lit/ds/symlink/ads1299.pdf
+
+
+#include "bcm2835.h"
+#include <stdint.h>
+#include "definitions.h"
+#include <stdio.h>
+#include "ads1299.h"
+
+
+// function call to set up all the communication between the raspberry pi bplus and the ADS1299 development board.
+// input: none
+// returns: 0 if everything inorder, else error code
+// note: make sure spi settings are the same
+// note: this function also resets the ads1299
+// note: this function also sends SDATAC to stop continuous read
+int initLibrary()
+{
+	if (!bcm2835_init())
+		return 24;
+
+	// Begin spi by initializing all the required pins
+	bcm2835_spi_begin();
+
+	// Set bit order
+    bcm2835_spi_setBitOrder(BCM2835_SPI_BIT_ORDER_MSBFIRST);      // The default
+    
+    // Set the spi mode - 4 modes
+	//	BCM2835_SPI_MODE1 = 1,  // CPOL = 0, CPHA = 1, Clock idle low, data is clocked in on falling edge, output data (change) on rising edge
+    bcm2835_spi_setDataMode(BCM2835_SPI_MODE1);                   // The default
+    
+	//Set SPI clock speed
+	//	BCM2835_SPI_CLOCK_DIVIDER_512   = 512,     ///< 512 = 2.048us = 488.28125kHz
+    bcm2835_spi_setClockDivider(BCM2835_SPI_CLOCK_DIVIDER_512); // The default
+
+    // Chip select
+    bcm2835_spi_chipSelect(BCM2835_SPI_CS0);
+    
+    // Select the polarity
+    // LOW or HIGH
+    bcm2835_spi_setChipSelectPolarity(BCM2835_SPI_CS0, HIGH);
+
+    // Set the pin_DRDY to be the input 
+    bcm2835_gpio_fsel(PIN_DRDY, BCM2835_GPIO_FSEL_INPT);
+
+    // Set the ss pin to output
+    bcm2835_gpio_fsel(PIN_CS, BCM2835_GPIO_FSEL_OUTP);
+    
+    // Set the ss pin to be high by default
+    bcm2835_gpio_write(PIN_CS, HIGH);
+
+	// --- RESET THE ADS1299 --- //
+	// Issue the ss pulse
+	// bcm2835_gpio_write(PIN_CS, LOW);
+    // wait for a minimum of 2*TCLK    
+    // bcm2835_delayMicroseconds(3*TCLK);
+	// Make ss go high again
+	// NOTE : Use the reset opcode (0x60) instead
+
+    bcm2835_gpio_write(PIN_CS, LOW);
+	bcm2835_delayMicroseconds(1000);
+
+	// reset opcode sent
+	transferData(0x60);
+    
+    // wait for min 18*TCLK for reset
+	bcm2835_delayMicroseconds(20*TCLK);
+
+	return 0;
+}
+
+// function to send/receive data after initialization
+// input: data to be sent on MOSI line
+// returns: data on MISO line
+// note: prints mosi, miso when DEBUG 1
+uint8_t transferData(uint8_t _data)
+{	
+	#ifdef __DEBUG__
+		printf("MOSI: %02x \n", _data);
+	#endif
+	
+	uint8_t recv = bcm2835_spi_transfer(_data);
+
+	#ifdef __DEBUG__
+		printf("MISO: %02x \n", recv);
+	#endif
+
+	return (recv);	
+}
+
+// function to send/receive data on spi for _nregs-1 number of registers
+// input: 
+// output: 
+/*uint8_t transferNData(uint8_t _data, int _nregs)
+{
+	uint8_t *data
+	for(i=0;i<_nregs;i++)
+	{
+
+	}
+
+}*/
+
+// function to transfer commands to the ads1299 - pg 35 of the datasheet
+// input: none
+// return: none
+void transferCmd(uint8_t _cmd)
+{
+	// just send _RESET 
+	transferData(_cmd);
+
+}
+
+
+// function to get the device id of ads1299
+// input: none
+// return: device id in hex
+// note: the device id should be 0x3e
+uint8_t getDeviceId()
+{
+	uint8_t deviceid;
+
+	// Calling _SDATAC for stopping continuous data mode
+	transferData(_SDATAC);
+
+	// Always wait for 4*TCLK after after _SDATAC passed
+	bcm2835_delayMicroseconds(4*TCLK);
+
+	// Calling _RREG for initializing read of registers
+	transferData(_RREG);
+
+	// Number of registers to be read 1
+	transferData(0x00);
+
+	// push in dummy bit to get device id
+	deviceid = transferData(0x00);
+
+	// restart the _RDATAC mode
+	// transferData(_RDATAC);
+
+	return deviceid;
+}
+
+// function to directly read/write to a register 
+// input: to read/write, register address, register value to write
+// output: the read data in case of read and 0x00 in case of a write
+uint8_t rregTransferData(int _readorwrite, uint8_t _rregadd, uint8_t _rregvalue)
+{
+	uint8_t data;
+
+	// read is 1, write is 0
+	if (_readorwrite)
+		data = 0x20;
+	else
+		data = 0x40;
+
+	data = data + _rregadd;
+
+	transferData(data);
+	transferData(0x00);
+
+	if (_readorwrite)
+		data = transferData(0x00);
+	else
+		transferData(_rregvalue);
+
+	return data;
+}
+
+void transferComplete()
+{
+    bcm2835_spi_end();
+    bcm2835_close();
+}
