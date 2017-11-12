5deae93e8fd77f33878d4891434107580076f76d
Ramin Ranjbar <raxbits@users.noreply.github.com>
2017-11-12 13:56:39 -0500

    Fix typo

--- code/software/ADS_1299_Embedded/main.c
+++ code/software/ADS_1299_Embedded/main.c
@@ -6,7 +6,7 @@ int main(int argc, char **argv)
 {
     // If error in intializing bcm
     if (!bcm2835_init())
-    {printf("Failed to fail the Raspberry PI");
+    {printf("Failed to init the Raspberry PI");
 	return 1;}
 	// Initialize BCM Configuration to communicate with chip
 	ADS1299_init();
