e26f7d40629b0aced746ba7e4ee8e2f46303bed2
a1bertle <albert.le6446@gmail.com>
2017-11-14 00:07:27 -0500

    Add test_registers function call to main

--- code/software/ADS_1299_Embedded/main.c
+++ code/software/ADS_1299_Embedded/main.c
@@ -1,18 +1,24 @@
 /*Include the needed libraries for functionality*/
-#include <stdout>
-
+#include "ADS1299_definitions.h"
+#include "ADS1299_bcm2835.h"
+#include "ads1299.h"
 
 int main(int argc, char **argv)
 {
     // If error in intializing bcm
-    if (!bcm2835_init())
-    {printf("Failed to init the Raspberry PI");
-	return 1;}
-	// Initialize BCM Configuration to communicate with chip
-	ADS1299_init();
+    if (!bcm2835_init()) {
+	printf("Failed to init the Raspberry PI");
+	return 1;
+    }
+
+    // Initialize BCM Configuration to communicate with chip
+    ADS1299_init();
 
     // bootup sequence
     ADS1299_bootup();
+
+    // Test registers' default values
+    ADS1299_test_registers(); 
  
     return 0;
 }
