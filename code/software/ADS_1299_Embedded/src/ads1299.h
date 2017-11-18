879d9ce228e9b904a3a8573b2222475474c1cba0
a1bertle <albert.le6446@gmail.com>
2017-11-17 20:11:49 -0500

    Fix compilation warnings

--- code/software/ADS_1299_Embedded/src/ads1299.h
+++ code/software/ADS_1299_Embedded/src/ads1299.h
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include "bcm2835.h"
 
 #ifndef _ADS1299_h
 #define _ADS1299_h
@@ -12,5 +13,6 @@ uint8_t transferData(uint8_t);
 void transferComplete();
 void transferCmd(uint8_t);
 uint8_t getDeviceId();
+uint8_t rregTransferData(int _readorwrite, uint8_t _rregadd, uint8_t _rregvalue);
 
 #endif
