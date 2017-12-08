8f85c4044134fb9e100d865fe79233681e4df4ac
a1bertle <albert.le6446@gmail.com>
2017-12-08 00:23:59 -0500

    Re-structure program to decode ADS1299 RREG command to imitate on-chip register interface

--- code/software/arduino/ads1299_arduino_emulator/ads1299_arduino_emulator.ino
+++ code/software/arduino/ads1299_arduino_emulator/ads1299_arduino_emulator.ino
@@ -1,25 +1,22 @@
 #include <SPI.h>
 #include <string.h>
 
-byte recv_buff[3];
-int pos = 0;
 byte process_it;
 
+// Register Read/Write settings
+bool read_reg, write_reg, rw_done;
+byte base_addr;
+int rw_offset;
+int n_regs;
+byte opcode;
+
+byte rec_byte;
 byte regs_defaults[5] = {0x1E, 0x96, 0xA0, 0x60, 0xFF};
 
-enum {WAKEUP, STANDBY, RESET, START, STOP, RDATAC, SDATAC, RDATA, RREG, WREG, _NULL, NOT_WREG_RREG};
+enum {WAKEUP, STANDBY, RESET, START, STOP, RDATAC, SDATAC, RDATA, RREG, WREG, NULL_STATE, NOT_WREG_RREG};
 
-int current_state;
+String current_state;
 int pin_reset = 8;
-
-// Instruction decoding
-byte opcode;
-byte byte_decoded;
-int address;
-String sys_command;
-bool req_reg_access = false;
-
-byte prev_byte;
 
 void setup() {
   Serial.begin(115200);
@@ -34,147 +31,88 @@ void setup() {
   SPI.attachInterrupt();
   SPDR = 0;
 
-}
+  read_reg = false;
+  write_reg = false;
+  rw_done = true;
+  base_addr = 0x00;
+  rw_offset = 0;
+  n_regs = 0;
 
+  current_state = "RDATAC";
+
+}
 
 void loop() {
-  
-  if (process_it) {
-      //Serial.println("Received command");
-      process_it = false;
-
-//    Serial.print("Raw: 0x");
-//    Serial.print(recv_buff[0], HEX);
-//    Serial.print(" 0x");
-//    Serial.println(recv_buff[1], HEX);
-//    Serial.print("Read or write: ");
-//    Serial.println(req_reg_access);
-//    Serial.print("Byte Decoded: 0x");
-//    Serial.println(byte_decoded, HEX);
-    
-//    Serial.print("Command: ");
-//    Serial.print(sys_command);
-//    Serial.print(" Address: ");
-//    Serial.println(address, HEX);
-//    Serial.println("");
-
-  }
-}
-
-// Depending on the command recieved from the Pi, send a known register value back to the Pi.
-ISR(SPI_STC_vect) {
-  byte rec_byte = SPDR;
-  process_it = false;
-  if (rec_byte == 0x20) {
-    prev_byte = 0x20;
-  } else if (rec_byte == 0x21) {
-    prev_byte = 0x21;
-  } else if (rec_byte == 0x22) {
-    prev_byte = 0x22;
-  } else if (rec_byte == 0x23) {
-    prev_byte = 0x23;
-  } else if (rec_byte == 0x00) {
-     process_it = true;
-     if (prev_byte == 0x20) {
-      SPDR = regs_defaults[0];
-    } else if (prev_byte == 0x21) {
-      SPDR = regs_defaults[1];
-    } else if (prev_byte == 0x22) {
-      SPDR = regs_defaults[2];
-    } else if (prev_byte == 0x23) {
-      SPDR = regs_defaults[3];  
-    } else {
-      process_it = false;
-    }
-    prev_byte = 0x00;
-  } else {
-    process_it = false;
-  }
-    
-//  if (pos == 0) 
-//    decode_opcode(rec_byte);
-//
-//  if (pos < sizeof recv_buff) {
-//    recv_buff[pos++] = rec_byte;
-//    SPDR = 0;
-//    process_it = false;
-//
-//    if (req_reg_access) {
-//      if (pos == 2) {
-//        address = rec_byte;
-//        SPDR = regs_defaults[address];
-//      } else if (pos == 3) { // Done accepting two-byte command
-//        pos = 0;
-//        process_it = true;
-//      } 
-//    } else { // Not WREG or RREG
-//      pos = 0;
-//      process_it = true;
-//    }
-//  } else {
-//    pos = 0;
-//    SPDR = 0;
+  // Do nothing for now, ISR takes too long to interrupt Serial 
+//  if (process_it) {
+//      Serial.print("Current State: ");
+//      Serial.print(current_state);
+//      Serial.print("  Received command: ");
+//      Serial.println(rec_byte);
+//      process_it = false;
 //  }
 }
 
-// Function currently takes too long to be run in ISR.
-void decode_opcode(byte _recv_opcode) {
-  // Extract opcode
-  byte first_byte = _recv_opcode;
-  byte_decoded = first_byte;
+// Depending on the command recieved from the Pi, send a known register value back to the Pi.
+// Only process Data Read commands and Register Read Commands 
+//  Data Read commands (single byte)
+//  RDATAC = 0b0001_0000
+//  SDATAC = 0b0001_0001
+//  RDATA  = 0b0001_0010
+// 
+//  Register Read Commands
+//  RREG   = 0b001r_rrrr 0b000n_nnnn
+//  WREG   = 0b010r_rrrr 0b000n_nnnn
+//
+// TODO: Add support for WREG and System Commands: WAKEUP, STANDBY, RESET, START, STOP
+ISR(SPI_STC_vect) {
+  rec_byte = SPDR;
 
-  // 3 MSB's are 0 so the command is not WREG or RREG
-  if (first_byte >> 5 == 0x00) {
-    req_reg_access = false;
-    address = 0x05;
-  } else {                         
-    req_reg_access = true;
-    address = _recv_opcode & 0x1F;
-    if (first_byte >> 5 == 0x1) {
-      opcode = RREG;
-      sys_command = "RREG";
-    } else if (first_byte >> 5 == 0x2) {
-      opcode = WREG;
-      sys_command = "WREG";
-    } else {
-      req_reg_access = false;
-      opcode = NOT_WREG_RREG;
-      sys_command = "NOT_WREG_RREG";
+  if (!read_reg && !write_reg) {
+    SPDR = 0x00;
+    if (((rec_byte >> 4) == 0x1) && !(rec_byte & 0xE6)) {        // Is a Data Read command
+      opcode = rec_byte & 0xF; // Isolate 4 lower bits
+      if (opcode == 0x0)
+        current_state = "RDATAC";
+      else if (opcode == 0x1)
+        current_state = "SDATAC";
+      else if (opcode == 0x2)
+        current_state = "RDATA";
+      else 
+        current_state = NULL_STATE;
+      process_it = true;
+    } else if ((rec_byte >> 5) == 0x1) { // Is a RREG command
+      read_reg = true;
+      base_addr = rec_byte & 0x1F;
+      rw_offset = 0;
+    } else if ((rec_byte >> 5) == 0x2) { // Is a WREG command
+      write_reg = true;
+      base_addr = rec_byte & 0x1F;
+      rw_offset = 0;
     }
-  }
+    process_it = true;
+  } else {                               // Previous byte was a RREG or WREG command
+      if (rw_done) {                     // If read_reg or write_reg are true and rw_done is high, this means we have not shifted any data out yet
+        n_regs = rec_byte & 0x1F;
+        rw_done = false;
+      } 
+      if (read_reg) {
+        SPDR = regs_defaults[base_addr + rw_offset];
+        rw_offset++;
+      } else if (write_reg) {
+        // Do nothing for now
+        rw_offset++;
+      }
 
-  if (!req_reg_access) {
-
-      if (first_byte == 0x2) {
-        opcode      = WAKEUP; 
-        sys_command = "WAKEUP";
-      } else if (first_byte == 0x4) {
-        opcode      = STANDBY;
-        sys_command = "STANDBY";
-      }else if (first_byte == 0x6) {
-        opcode      = RESET;
-        sys_command = "RESET";
-      } else if (first_byte == 0x8) {
-        opcode      = START;
-        sys_command = "START";
-      } else if (first_byte == 0xA) {
-        opcode      = STOP;
-        sys_command = "STOP";
-      } else if (first_byte == 0x10) {
-        opcode      = RDATAC;
-        sys_command = "RDATAC";
-      } else if (first_byte == 0x11) {
-        opcode      = SDATAC;
-        sys_command = "SDATAC";
-      } else if (first_byte == 0x12) {
-        opcode      = RDATA;
-        sys_command = "RDATA";
-      } else {
-        opcode      = _NULL;
-        sys_command = "NULL";
+      if (rw_offset > n_regs) {          // Finished reading registers, reset all flags to defaults and set rw_done high
+        read_reg = false;
+        write_reg = false;
+        rw_done = true;
+        base_addr = 0x00;
+        rw_offset = 0;  
+        n_regs = 0;
       }
   }
 }
-
 
 
